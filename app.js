a6073590d39e53149f5043d5648b018e27b88de5
sc-designs <152136634+Sc-Designs@users.noreply.github.com>
2025-08-12 10:36:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", AiRouter);
+app.use("/",(req, res)=>{
+  res.send("Health Check");
+});
+app.use("/api", AiRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
